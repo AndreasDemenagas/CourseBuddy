578808224cff90414c8d864be401da4ba258c6dc
AndreasDemenagas <ademenagas01@gmail.com>
2020-12-22 00:53:46 +0200

    TabBar created

---
+++ CourseBuddy/Constants/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  CourseBuddy
+//
+//  Created by Andrew Demenagas on 22/12/20.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Images {
+        
+        static let coursesTabBar = "books.vertical.fill"
+        static let calendarTabBar = "calendar"
+        static let groupsTabBar = "person.3.fill"
+        static let casTabBar = "pencil.and.outline"
+    }
+    
+    
+    
+    
+}
---
+++ CourseBuddy/MainTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabBarController.swift
+//  CourseBuddy
+//
+//  Created by Andrew Demenagas on 22/12/20.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .systemGreen
+        tabBar.barStyle = .black
+        
+        let coursesController = navigationController(viewController: UIViewController(), imageName: Constants.Images.coursesTabBar, title: "Courses")
+        let deadlinesController = navigationController(viewController: UIViewController(), imageName: Constants.Images.calendarTabBar, title: "Deadlines")
+        let casController = navigationController(viewController: UIViewController(), imageName: Constants.Images.casTabBar, title: "CAS")
+        let groupsController = navigationController(viewController: UIViewController(), imageName: Constants.Images.groupsTabBar, title: "Groups")
+        
+        viewControllers = [
+            coursesController,
+            deadlinesController,
+            casController,
+            groupsController
+        ]
+    }
+    
+    private func navigationController(viewController: UIViewController, imageName: String, title: String) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: viewController)
+        viewController.navigationItem.title = title
+        viewController.tabBarItem.title = title
+        //viewController.navigationController?.navigationBar.prefersLargeTitles = true
+        viewController.tabBarItem.image = UIImage(systemName: imageName)
+        return navigationController
+    }
+    
+}
